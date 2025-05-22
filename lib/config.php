69d23160cbe50037ff2fe3758eb59cedf36b7ce2
Christelle Cossard <christelle.cossard10@gmail.com>
2025-05-22 08:33:22 +0200

    config

--- lib/config.php
+++ lib/config.php
@@ -4,3 +4,9 @@ define('_ASSETS_IMAGES_FOLDER_', '/images/');
 
 require_once ('secret.php');
 
+/*
+define("_DB_SERVER_", "");
+define("_DB_NAME_", "");
+define("_DB_USER_", "");
+define("_DB_PASSWORD_", "");
+*/
