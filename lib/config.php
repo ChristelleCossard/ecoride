4eff93412471a83718d14a87cf9d79b414dbc8de
Christelle Cossard <christelle.cossard10@gmail.com>
2025-05-22 08:25:45 +0200

    retrait identifiants

--- lib/config.php
+++ lib/config.php
@@ -2,8 +2,5 @@
 
 define('_ASSETS_IMAGES_FOLDER_', '/images/');
 
-define("_DB_SERVER_", "mysql-cocodev.alwaysdata.net");
-define("_DB_NAME_", "cocodev_ecoride");
-define("_DB_USER_", "cocodev");
-define("_DB_PASSWORD_", "Vicky.10");
+require_once ('secret.php');
 
